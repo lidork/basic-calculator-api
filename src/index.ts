5790f555bb54c08a7b146395b7e8b621416990e5
Lidor Kalfon <lidor.kalfon@gmail.com>
2025-05-13 19:09:15 +0300

    functions

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,23 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+function add(a: number, b: number) : number {
+  return a + b;
+}
+
+function subtract(a: number, b: number) : number {
+  return a - b;
+}
+
+function multiply (a: number, b: number): number {
+  return a * b;
+}
+
+function divide (a: number, b: number) : number | string {
+  if ( b == 0 )
+    return 'error: Cannot divide by zero'
+  return a/b;
+}
 app.get('/', (_req: Request, res: Response) => {
   res.send('Calculator API is running!');
 });
@@ -10,13 +27,13 @@ app.get('/', (_req: Request, res: Response) => {
 app.get('/add', (req: Request, res: Response) => {
   const a = Number(req.query.a);
   const b = Number(req.query.b);
-  res.json({ result: a + b });
+  res.json({ result: add(a,b) });
 });
 
 app.get('/subtract', (req: Request, res: Response) => {
   const a = Number(req.query.a);
   const b = Number(req.query.b);
-  res.json({ result: a - b });
+  res.json({ result: subtract(a,b) });
 });
 
 app.get('/multiply', (req: Request, res: Response) => {
@@ -28,13 +45,11 @@ app.get('/multiply', (req: Request, res: Response) => {
 app.get('/divide', (req: Request, res: Response) => {
     const a = Number(req.query.a);
     const b = Number(req.query.b);
-
-    if (b === 0) {
-      res.status(400).json({ error: 'Cannot divide by zero' });
-      return;
-    }
-    
-    res.json({ result: a / b });
+    let c = divide(a,b);
+    if (typeof c != 'number')
+      res.status(400).json({error: 'Cannot divide by zero'});
+    else
+      res.json({ result: a / b });
 });
 
 app.listen(port, () => {
